b6d9323c90f73167fa9b87ea6a95558ad940395f
ycsal <yascsalgado@gmail.com>
2025-09-08 13:46:14 -0300

    refatorando o timer

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { ActionButton } from "../components/ActionButton";
 import { FokusButton } from "../components/FokusButton";
+import { Timer } from "../components/Timer";
 
 const pomodoro = [
   {
@@ -43,9 +44,7 @@ export default function Index() {
             />
           ))}
         </View>
-        <Text style={styles.timer}>
-          { new Date(timerType.initialValue*1000).toLocaleTimeString('pt-BR', { minute: '2-digit', second: '2-digit' }) }
-        </Text>
+        <Timer totalSeconds={timerType.initialValue} />
         <FokusButton />
       </View>
       <View style={styles.footer}>
@@ -78,12 +77,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: 'space-around',
     alignItems: 'center'
-  },
-  timer: {
-    fontSize: 54,
-    color: '#FFF',
-    fontWeight: 'bold',
-    textAlign: 'center'
   },
   footer: {
     width: '80%',
